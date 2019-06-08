5eac8186c39658ccd561dd45be781462f6fe4dc3
Mahtab Alam <Mahtab.Alam@ge.com>
2019-06-08 13:05:08 +0530

    Added Counting sort document

--- src/net/mahtabalam/CountingSort.java
+++ src/net/mahtabalam/CountingSort.java
@@ -5,34 +5,40 @@ import java.util.*;
 class CountingSort {
 
 	public static void main(String[] args) {
-//		int[] arr = {-5, -10, 0, -3, 8, 5, -1, 10}; 
-//		int[] arr = { 9, 31, 10, 1, 20, 17, 45, 39, 33, 3, 20, 28, 27, 17 };
-		int[] arr = {10, 7, 12, 4, 9, 13}; 
-		int output[] = countSort(arr);
+		// int[] arr = { 9, 31, 10, 1, 20, 17, 45, 39, 33, 3, 20, 28, 27, 17 };
+		 int[] arr = {10, 7, 12, 4, 9, 13};
+		// int[] arr = { 4, 0, 0, 1, 0, 2, 4, 5, 1 };
 		System.out.println("Input Array  :" + Arrays.toString(arr));
-		System.out.println("Sorted Array :" + Arrays.toString(output));
+		countSort(arr);
+		System.out.println("Sorted Array :" + Arrays.toString(arr));
 	}
 
-	public static int[] countSort(int[] arr) {
-		int max = Arrays.stream(arr).max().getAsInt();
-		int min = Arrays.stream(arr).min().getAsInt();
+	public static void countSort(int[] arr) {
+		int max = Arrays.stream(arr).max().getAsInt(); // Java 8 - Retrieving Maximum number
+		int min = Arrays.stream(arr).min().getAsInt(); // Java 8 - Retrieving Minimum number
 		int range = max - min + 1;
 		int count[] = new int[range];
 		int output[] = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
+
+		for (int i = 0; i < arr.length; i++) { // First Loop
 			count[arr[i] - min] = count[arr[i] - min] + 1;
 		}
+		System.out.println("Initial Count Array :" + Arrays.toString(count));
 
-		for (int i = 1; i < count.length; i++) {
+		for (int i = 1; i < count.length; i++) { // Second Loop
 			count[i] = count[i] + count[i - 1];
 		}
 
-		for (int i = 0; i < arr.length; i++) {
+		System.out.println("Summing the counts :" + Arrays.toString(count));
+
+		for (int i = 0; i < arr.length; i++) { // Third Loop
 			output[count[arr[i] - min] - 1] = arr[i];
 			count[arr[i] - min] = count[arr[i] - min] - 1;
 		}
-		
-		return output;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = output[i];
+		}
 	}
 
 }
